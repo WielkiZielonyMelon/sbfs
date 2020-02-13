53f24a2450764fa95694e2565e5af6853485ba22
Melon <wielkizielonymelon@protonmail.com>
2020-02-13 12:28:45 +0100

    Further test parametrization

--- test_board_get_legal_moves.cpp
+++ test_board_get_legal_moves.cpp
@@ -47,13 +47,13 @@ TEST_CASE("Test legal moves on empty 2x2 board") {
     using tuple_type = tuple<Position, vector<Position>>;
     auto dump = GENERATE(table<Position, vector<Position>>( {
                 tuple_type{ Position(0, 0), vector<Position> {
-                                Position(0, 1), Position(1, 0) }},
+                            Position(0, 1), Position(1, 0) }},
                 tuple_type{ Position(0, 1), vector<Position> {
-                                Position(0, 0), Position(1, 1) }},
+                            Position(0, 0), Position(1, 1) }},
                 tuple_type{ Position(1, 1), vector<Position> {
-                                Position(1, 0), Position(0, 1) }},
+                            Position(1, 0), Position(0, 1) }},
                 tuple_type{ Position(1, 0), vector<Position> {
-                                Position(0, 0), Position(1, 1) }}
+                            Position(0, 0), Position(1, 1) }}
                 }));
     auto startPos = get<0>(dump);
     auto expectedMoves = get<1>(dump);
@@ -65,108 +65,38 @@ TEST_CASE("Test legal moves on empty 2x2 board") {
     REQUIRE(legalMoves == expectedMoves);
 }
 
-TEST_CASE("Test legal moves on empty 3x3 board, start 0 0") {
+TEST_CASE("Test legal moves on empty 3x3 board") {
     Board board(3, 3);
 
-    auto startPos = Position(0, 0);
+    using tuple_type = tuple<Position, vector<Position>>;
+    auto dump = GENERATE(table<Position, vector<Position>>( {
+                tuple_type{ Position(0, 0), vector<Position> {
+                            Position(0, 1), Position(1, 0) }},
+                tuple_type{ Position(0, 1), vector<Position> {
+                            Position(0, 0), Position(1, 1), Position(0, 2) }},
+                tuple_type{ Position(1, 1), vector<Position> {
+                            Position(1, 0), Position(0, 1), Position(2, 1),
+                            Position(1, 2)}},
+                tuple_type{ Position(1, 0), vector<Position> {
+                            Position(0, 0), Position(1, 1), Position(2, 0) }},
+                tuple_type{ Position(2, 0), vector<Position> {
+                            Position(1, 0), Position(2, 1) }},
+                tuple_type{ Position(2, 1), vector<Position> {
+                            Position(2, 0), Position(2, 2), Position(1, 1) }},
+                tuple_type{ Position(0, 2), vector<Position> {
+                            Position(0, 1), Position(1, 2) }},
+                tuple_type{ Position(1, 2), vector<Position> {
+                            Position(0, 2), Position(1, 1), Position(2, 2) }},
+                tuple_type{ Position(2, 2), vector<Position> {
+                            Position(1, 2), Position(2, 1) }}
+                }));
+    auto startPos = get<0>(dump);
+    auto expectedMoves = get<1>(dump);
     auto legalMoves = board.getLegalMoves(startPos);
-    auto expectedMoves = vector<Position>{Position(1, 0), Position(0, 1)};
+
     sort(legalMoves.begin(), legalMoves.end());
     sort(expectedMoves.begin(), expectedMoves.end());
+
     REQUIRE(legalMoves == expectedMoves);
 }
 
-TEST_CASE("Test legal moves on empty 3x3 board, start 1 0") {
-    Board board(3, 3);
-
-    auto startPos = Position(1, 0);
-    auto legalMoves = board.getLegalMoves(startPos);
-    auto expectedMoves = vector<Position>{Position(0, 0), Position(1, 1),
-                                          Position(2, 0)};
-    sort(legalMoves.begin(), legalMoves.end());
-    sort(expectedMoves.begin(), expectedMoves.end());
-    REQUIRE(legalMoves == expectedMoves);
-}
-
-TEST_CASE("Test legal moves on empty 3x3 board, start 2 0") {
-    Board board(3, 3);
-
-    auto startPos = Position(2, 0);
-    auto legalMoves = board.getLegalMoves(startPos);
-    auto expectedMoves = vector<Position>{Position(1, 0),
-                                          Position(2, 1)};
-    sort(legalMoves.begin(), legalMoves.end());
-    sort(expectedMoves.begin(), expectedMoves.end());
-    REQUIRE(legalMoves == expectedMoves);
-}
-
-TEST_CASE("Test legal moves on empty 3x3 board, start 2 1") {
-    Board board(3, 3);
-
-    auto startPos = Position(2, 1);
-    auto legalMoves = board.getLegalMoves(startPos);
-    auto expectedMoves = vector<Position>{Position(2, 2), Position(1, 1),
-                                          Position(2, 0)};
-    sort(legalMoves.begin(), legalMoves.end());
-    sort(expectedMoves.begin(), expectedMoves.end());
-    REQUIRE(legalMoves == expectedMoves);
-}
-
-TEST_CASE("Test legal moves on empty 3x3 board, start 0 1") {
-    Board board(3, 3);
-
-    auto startPos = Position(0, 1);
-    auto legalMoves = board.getLegalMoves(startPos);
-    auto expectedMoves = vector<Position>{Position(0, 0), Position(1, 1),
-                                          Position(0, 2)};
-    sort(legalMoves.begin(), legalMoves.end());
-    sort(expectedMoves.begin(), expectedMoves.end());
-    REQUIRE(legalMoves == expectedMoves);
-}
-
-TEST_CASE("Test legal moves on empty 3x3 board, start 0 2") {
-    Board board(3, 3);
-
-    auto startPos = Position(0, 2);
-    auto legalMoves = board.getLegalMoves(startPos);
-    auto expectedMoves = vector<Position>{Position(0, 1),
-                                          Position(1, 2)};
-    sort(legalMoves.begin(), legalMoves.end());
-    sort(expectedMoves.begin(), expectedMoves.end());
-    REQUIRE(legalMoves == expectedMoves);
-}
-
-TEST_CASE("Test legal moves on empty 3x3 board, start 1 2") {
-    Board board(3, 3);
-
-    auto startPos = Position(1, 2);
-    auto legalMoves = board.getLegalMoves(startPos);
-    auto expectedMoves = vector<Position>{Position(0, 2), Position(1, 1),
-                                          Position(2, 2)};
-    sort(legalMoves.begin(), legalMoves.end());
-    sort(expectedMoves.begin(), expectedMoves.end());
-    REQUIRE(legalMoves == expectedMoves);
-}
-
-TEST_CASE("Test legal moves on empty 3x3 board, start 1 1") {
-    Board board(3, 3);
-
-    auto startPos = Position(1, 1);
-    auto legalMoves = board.getLegalMoves(startPos);
-    auto expectedMoves = vector<Position>{Position(1, 0), Position(0, 1),
-                                          Position(2, 1), Position(1, 2)};
-    sort(legalMoves.begin(), legalMoves.end());
-    sort(expectedMoves.begin(), expectedMoves.end());
-    REQUIRE(legalMoves == expectedMoves);
-}
-
-TEST_CASE("Test legal moves on empty 3x3 board, start 2 2") {
-    Board board(3, 3);
-
-    auto startPos = Position(2, 2);
-    auto legalMoves = board.getLegalMoves(startPos);
-    auto expectedMoves = vector<Position>{Position(2, 1), Position(1, 2)};
-    sort(legalMoves.begin(), legalMoves.end());
-    sort(expectedMoves.begin(), expectedMoves.end());
-    REQUIRE(legalMoves == expectedMoves);
-}
