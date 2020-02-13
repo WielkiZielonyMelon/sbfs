3cc786a8c283e9ff5aa05d165ffc7ceccf62fceb
Melon <wielkizielonymelon@protonmail.com>
2020-02-13 14:48:22 +0100

    Added test for getting moves if there are walls around

---
+++ test_get_legal_moves_with_walls.cpp
@@ -0,0 +1,167 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include <algorithm>
+#include <tuple>
+#include <vector>
+#include "board.h"
+#include "types.h"
+
+using std::get;
+using std::sort;
+using std::tuple;
+using std::vector;
+
+TEST_CASE("Test legal moves on empty 2x1 board") {
+    Board board(2, 1);
+    board[0][0] = Tile::Wall;
+
+    auto startPos = Position(1, 0);
+    auto expectedMoves = vector<Position>();
+    auto legalMoves = board.getLegalMoves(startPos);       
+    REQUIRE(legalMoves == expectedMoves);
+
+    board[1][0] = Tile::Wall;
+    startPos = Position(0, 0);
+    legalMoves = board.getLegalMoves(startPos);       
+    REQUIRE(legalMoves == expectedMoves);
+}
+
+TEST_CASE("Test legal moves on empty 1x2 board") {
+    Board board(1, 2);
+    board[0][0] = Tile::Wall;
+
+    auto startPos = Position(0, 1);
+    auto expectedMoves = vector<Position>();
+    auto legalMoves = board.getLegalMoves(startPos);       
+    REQUIRE(legalMoves == expectedMoves);
+
+    board[0][1] = Tile::Wall;
+    startPos = Position(0, 0);
+    legalMoves = board.getLegalMoves(startPos);       
+    REQUIRE(legalMoves == expectedMoves);
+}
+
+TEST_CASE("Test legal moves on empty 2x2 board") {
+    Board board(2, 2);
+
+    using tuple_type = tuple<Position, vector<Position>, vector<Position>>;
+    auto dump = GENERATE(table<Position, vector<Position>, vector<Position>>( {
+                tuple_type{ Position(0, 0),
+                            vector<Position> { Position(1, 0) },
+                            vector<Position> { Position(0, 1) }},
+                tuple_type{ Position(0, 0),
+                            vector<Position> { Position(1, 0) },
+                            vector<Position> { Position(0, 1) }},
+                tuple_type{ Position(0, 0),
+                            vector<Position> { Position(0, 1), Position(1, 0) },
+                            vector<Position> { }},
+                tuple_type{ Position(0, 1),
+                            vector<Position> { Position(0, 0) },
+                            vector<Position> { Position(1, 1) }},
+                tuple_type{ Position(0, 1),
+                            vector<Position> { Position(1, 1) },
+                            vector<Position> { Position(0, 0) }},
+                tuple_type{ Position(0, 1),
+                            vector<Position> { Position(0, 0), Position(1, 1) },
+                            vector<Position> { }},
+                tuple_type{ Position(1, 0),
+                            vector<Position> { Position(0, 0) },
+                            vector<Position> { Position(1, 1) }},
+                tuple_type{ Position(1, 0),
+                            vector<Position> { Position(1, 1) },
+                            vector<Position> { Position(0, 0) }},
+                tuple_type{ Position(1, 0),
+                            vector<Position> { Position(1, 1), Position(0, 0) },
+                            vector<Position> { }},
+                tuple_type{ Position(1, 1),
+                            vector<Position> { Position(0, 1) },
+                            vector<Position> { Position(1, 0) }},
+                tuple_type{ Position(1, 1),
+                            vector<Position> { Position(1, 0) },
+                            vector<Position> { Position(0, 1) }},
+                tuple_type{ Position(1, 1),
+                            vector<Position> { Position(1, 0), Position(0, 1) },
+                            vector<Position> { }}
+                }));
+    auto startPos = get<0>(dump);
+    auto wallPos = get<1>(dump);
+    auto expectedMoves = get<2>(dump);
+    for (auto wall: wallPos) {
+        board[get<0>(wall)][get<1>(wall)] = Tile::Wall;
+    }
+
+    auto legalMoves = board.getLegalMoves(startPos);
+
+    sort(legalMoves.begin(), legalMoves.end());
+    sort(expectedMoves.begin(), expectedMoves.end());
+
+    REQUIRE(legalMoves == expectedMoves);
+}
+
+TEST_CASE("Test legal moves on empty 3x3 board") {
+    Board board(3, 3);
+
+    using tuple_type = tuple<Position, vector<Position>, vector<Position>>;
+    auto dump = GENERATE(table<Position, vector<Position>, vector<Position>>( {
+                tuple_type{ Position(0, 0),
+                            vector<Position> { Position(1, 0) },
+                            vector<Position> { Position(0, 1) }},
+                tuple_type{ Position(0, 0),
+                            vector<Position> { Position(1, 0) },
+                            vector<Position> { Position(0, 1) }},
+                tuple_type{ Position(0, 0),
+                            vector<Position> { Position(0, 1), Position(1, 0) },
+                            vector<Position> { }},
+                tuple_type{ Position(0, 1),
+                            vector<Position> { Position(0, 0) },
+                            vector<Position> { Position(1, 1), Position(0, 2) }},
+                tuple_type{ Position(0, 1),
+                            vector<Position> { Position(1, 1) },
+                            vector<Position> { Position(0, 0), Position(0, 2) }},
+                tuple_type{ Position(0, 1),
+                            vector<Position> { Position(0, 2) },
+                            vector<Position> { Position(0, 0), Position(1, 1) }},
+                tuple_type{ Position(0, 1),
+                            vector<Position> { Position(0, 0), Position(1, 1) },
+                            vector<Position> { Position(0, 2)}},
+                tuple_type{ Position(1, 0),
+                            vector<Position> { Position(0, 0) },
+                            vector<Position> { Position(1, 1), Position(2, 0) }},
+                tuple_type{ Position(1, 0),
+                            vector<Position> { Position(1, 1) },
+                            vector<Position> { Position(0, 0), Position(2, 0) }},
+                tuple_type{ Position(1, 0),
+                            vector<Position> { Position(2, 0) },
+                            vector<Position> { Position(0, 0), Position(1, 1) }},
+                tuple_type{ Position(1, 0),
+                            vector<Position> { Position(0, 0), Position(1, 1) },
+                            vector<Position> { Position(2, 0)}},
+                tuple_type{ Position(1, 1),
+                            vector<Position> { Position(0, 1), Position(2, 1) },
+                            vector<Position> { Position(1, 0), Position(1, 2) }},
+                tuple_type{ Position(1, 1),
+                            vector<Position> { Position(1, 0), Position(1, 2) },
+                            vector<Position> { Position(0, 1), Position(2, 1) }},
+                tuple_type{ Position(1, 1),
+                            vector<Position> { Position(2, 1), Position(1, 2) },
+                            vector<Position> { Position(0, 1), Position(1, 0) }},
+                tuple_type{ Position(1, 1),
+                            vector<Position> { Position(0, 1), Position(1, 0) },
+                            vector<Position> { Position(2, 1), Position(1, 2) }},
+                }));
+    auto startPos = get<0>(dump);
+    auto wallPos = get<1>(dump);
+    auto expectedMoves = get<2>(dump);
+    for (auto wall: wallPos) {
+        board[get<0>(wall)][get<1>(wall)] = Tile::Wall;
+    }
+
+    auto legalMoves = board.getLegalMoves(startPos);
+
+    sort(legalMoves.begin(), legalMoves.end());
+    sort(expectedMoves.begin(), expectedMoves.end());
+
+    REQUIRE(legalMoves == expectedMoves);
+}
+
