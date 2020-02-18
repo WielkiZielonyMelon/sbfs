c414d599fc664fd746adbec18b463913dde341fb
Melon <wielkizielonymelon@protonmail.com>
2020-02-18 14:08:02 +0100

    Refactor of tests

--- test_get_legal_moves_empty_board.cpp
+++ test_get_legal_moves_empty_board.cpp
@@ -2,15 +2,17 @@
 #include "catch.hpp"
 
 #include <algorithm>
-#include <tuple>
 #include <vector>
 #include "board.h"
 #include "types.h"
 
-using std::get;
 using std::sort;
-using std::tuple;
 using std::vector;
+
+struct TestData {
+    Position startPos;
+    vector<Position> expectedMoves;
+};
 
 TEST_CASE("Test legal moves on empty 1x1 board") {
     Board board(1, 1);
@@ -23,52 +25,52 @@ TEST_CASE("Test legal moves on empty 2x1 board") {
     Board board(2, 1);
 
     auto tileType = GENERATE(Tile::Corridor, Tile::Storage);
-    auto dump = GENERATE(vector<Position>{ Position(0, 0), Position(1, 0) },    
-                         vector<Position>{ Position(1, 0), Position(0, 0) }); 
+    auto testData = GENERATE(TestData{Position(0, 0),
+                                 vector<Position>{Position(1, 0)}},
+                             TestData{Position(1, 0),
+                                 vector<Position>{Position(0, 0)}});
     board[0][0] = tileType;
     board[1][0] = tileType;
-    auto startPos = dump[0];
-    auto expectedMoves = dump[1];       
+    auto startPos = testData.startPos;
+    auto expectedMoves = testData.expectedMoves;
     auto legalMoves = board.getLegalMoves(startPos);       
-    REQUIRE(legalMoves[0] == expectedMoves);
+    REQUIRE(legalMoves == expectedMoves);
 }
 
 TEST_CASE("Test legal moves on empty 1x2 board") {
     Board board(1, 2);
 
     auto tileType = GENERATE(Tile::Corridor, Tile::Storage);
-    auto dump = GENERATE(vector<Position>{ Position(0, 0), Position(0, 1) },    
-                         vector<Position>{ Position(0, 1), Position(0, 0) }); 
+    auto testData = GENERATE(TestData{Position(0, 0),
+                                 vector<Position>{Position(0, 1)}},    
+                             TestData{Position(0, 1),
+                                 vector<Position>{Position(0, 0)}}); 
     board[0][0] = tileType;
     board[0][1] = tileType;
-    auto startPos = dump[0];
-    auto expectedMoves = dump[1];       
+    auto startPos = testData.startPos;
+    auto expectedMoves = testData.expectedMoves;
     auto legalMoves = board.getLegalMoves(startPos);       
-    REQUIRE(legalMoves[0] == expectedMoves);
+    REQUIRE(legalMoves == expectedMoves);
 }
 
 TEST_CASE("Test legal moves on empty 2x2 board") {
     Board board(2, 2);
 
     auto tileType = GENERATE(Tile::Corridor, Tile::Storage);
-    using tuple_type = tuple<Position, vector<Position>>;
-    auto dump = GENERATE(table<Position, vector<Position>>( {
-                tuple_type{ Position(0, 0), vector<Position> {
-                            Position(0, 1), Position(1, 0) }},
-                tuple_type{ Position(0, 1), vector<Position> {
-                            Position(0, 0), Position(1, 1) }},
-                tuple_type{ Position(1, 1), vector<Position> {
-                            Position(1, 0), Position(0, 1) }},
-                tuple_type{ Position(1, 0), vector<Position> {
-                            Position(0, 0), Position(1, 1) }}
-                }));
-    // TODO: Rewrite nicely
-    board[0][0] = tileType;
-    board[0][1] = tileType;
-    board[1][0] = tileType;
-    board[1][1] = tileType;
-    auto startPos = get<0>(dump);
-    auto expectedMoves = get<1>(dump);
+    auto testData = GENERATE(TestData{Position(0, 0), vector<Position> {
+                                 Position(0, 1), Position(1, 0)}},
+                             TestData{Position(0, 1), vector<Position> {
+                                 Position(0, 0), Position(1, 1) }},
+                             TestData{Position(1, 1), vector<Position> {
+                                 Position(1, 0), Position(0, 1) }},
+                             TestData{Position(1, 0), vector<Position> {
+                                 Position(0, 0), Position(1, 1) }});
+    for (unsigned int row = 0; row < 2; ++row)
+        for (unsigned int col = 0; col < 2; ++col)
+            board[row][col] = tileType;
+
+    auto startPos = testData.startPos;
+    auto expectedMoves = testData.expectedMoves;
     auto legalMoves = board.getLegalMoves(startPos);
 
     sort(legalMoves.begin(), legalMoves.end());
@@ -81,40 +83,35 @@ TEST_CASE("Test legal moves on empty 3x3 board") {
     Board board(3, 3);
 
     auto tileType = GENERATE(Tile::Corridor, Tile::Storage);
-    using tuple_type = tuple<Position, vector<Position>>;
-    auto dump = GENERATE(table<Position, vector<Position>>( {
-                tuple_type{ Position(0, 0), vector<Position> {
-                            Position(0, 1), Position(1, 0) }},
-                tuple_type{ Position(0, 1), vector<Position> {
-                            Position(0, 0), Position(1, 1), Position(0, 2) }},
-                tuple_type{ Position(1, 1), vector<Position> {
-                            Position(1, 0), Position(0, 1), Position(2, 1),
-                            Position(1, 2)}},
-                tuple_type{ Position(1, 0), vector<Position> {
-                            Position(0, 0), Position(1, 1), Position(2, 0) }},
-                tuple_type{ Position(2, 0), vector<Position> {
-                            Position(1, 0), Position(2, 1) }},
-                tuple_type{ Position(2, 1), vector<Position> {
-                            Position(2, 0), Position(2, 2), Position(1, 1) }},
-                tuple_type{ Position(0, 2), vector<Position> {
-                            Position(0, 1), Position(1, 2) }},
-                tuple_type{ Position(1, 2), vector<Position> {
-                            Position(0, 2), Position(1, 1), Position(2, 2) }},
-                tuple_type{ Position(2, 2), vector<Position> {
-                            Position(1, 2), Position(2, 1) }}
-                }));
-    // TODO: Rewrite nicely
-    board[0][0] = tileType;
-    board[0][1] = tileType;
-    board[0][2] = tileType;
-    board[1][0] = tileType;
-    board[1][1] = tileType;
-    board[1][2] = tileType;
-    board[2][0] = tileType;
-    board[2][1] = tileType;
-    board[2][2] = tileType;
-    auto startPos = get<0>(dump);
-    auto expectedMoves = get<1>(dump);
+    auto testData = GENERATE(TestData{Position(0, 0), vector<Position> {
+                                 Position(0, 1), Position(1, 0)}},
+                             TestData{Position(0, 1), vector<Position> {
+                                 Position(0, 0), Position(1, 1),
+                                 Position(0, 2) }},
+                             TestData{Position(1, 1), vector<Position> {
+                                 Position(1, 0), Position(0, 1),
+                                 Position(2, 1), Position(1, 2)}},
+                             TestData{Position(1, 0), vector<Position> {
+                                 Position(0, 0), Position(1, 1),
+                                 Position(2, 0) }},
+                             TestData{Position(2, 0), vector<Position> {
+                                 Position(1, 0), Position(2, 1) }},
+                             TestData{Position(2, 1), vector<Position> {
+                                 Position(2, 0), Position(2, 2),
+                                 Position(1, 1) }},
+                             TestData{Position(0, 2), vector<Position> {
+                                 Position(0, 1), Position(1, 2) }},
+                             TestData{Position(1, 2), vector<Position> {
+                                 Position(0, 2), Position(1, 1),
+                                 Position(2, 2) }},
+                             TestData{Position(2, 2), vector<Position> {
+                                 Position(1, 2), Position(2, 1) }});
+    for (unsigned int row = 0; row < 3; ++row)
+        for (unsigned int col = 0; col < 3; ++col)
+            board[row][col] = tileType;
+
+    auto startPos = testData.startPos;
+    auto expectedMoves = testData.expectedMoves;
     auto legalMoves = board.getLegalMoves(startPos);
 
     sort(legalMoves.begin(), legalMoves.end());
